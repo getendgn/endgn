b785b4fa0952ab845e8021b0526a4e660ed9edd9
nat <natnael08@protonmail.com>
2024-03-23 17:14:10 +0300

    update metricool.py

--- metricool.py
+++ metricool.py
@@ -5,23 +5,32 @@ API_URL = "https://app.metricool.com/api"
 
 
 def schedule_metricool_post(blog_id, user_id, post_data):
-    url = f"{API_URL}/v2/scheduler/posts?blogId={blog_id}&userId={user_id}"
-    headers = {
-        "Content-Type": "application/json",
-        "X-Mc-Auth": USER_TOKEN,
-    }
-    return requests.post(url, json=post_data, headers=headers)
+    url = f"{API_URL}/v2/scheduler/posts"
+    params = {"blogId": blog_id, "userId": user_id, "userToken": USER_TOKEN}
+    headers = {"Content-Type": "application/json"}
+
+    return requests.post(url, json=post_data, headers=headers, params=params)
 
 
 def create_metricool_list_post(blog_id, user_id, list_id):
-    url = f"{API_URL}/lists/posts/create?blogId={blog_id}&userId={user_id}&listid={list_id}&position=0&userToken={USER_TOKEN}"
-    return requests.get(url)
+    url = f"{API_URL}/lists/posts/create"
+    return requests.get(
+        url,
+        params={
+            "blogId": blog_id,
+            "userId": user_id,
+            "listid": list_id,
+            "position": 0,
+            "userToken": USER_TOKEN,
+        },
+    )
 
 
 def update_metricool_list_post(
     blog_id, user_id, list_id, post_id, post_text, media_urls
 ):
-    url = f"{API_URL}/lists/posts/updatepostlist?blogId={blog_id}&userId={user_id}&userToken={USER_TOKEN}"
+    url = f"{API_URL}/lists/posts/updatepostlist"
+    params = {"blogId": blog_id, "userId": user_id, "userToken": USER_TOKEN}
 
     payload = {
         "listid": (None, list_id),
@@ -30,4 +39,4 @@ def update_metricool_list_post(
         "pictures": (None, str(media_urls)),
     }
 
-    return requests.post(url, files=payload)
+    return requests.post(url, files=payload, params=params)
