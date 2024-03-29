5e23ea82fe4f578789797acd8f724f4970059fd9
nat <natnael08@protonmail.com>
2024-03-29 12:10:35 +0300

    update transcription.py

--- transcription.py
+++ transcription.py
@@ -1,8 +1,7 @@
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
-from datetime import datetime
-import os, time
 from openai import OpenAI
+import os, random, string
 
 
 def create_audio_chunks(video_path):
@@ -23,28 +22,26 @@ def create_audio_chunks(video_path):
             output_chunks.append(chunk)
 
     chunk_paths = []
+    title = "".join(
+        [random.choice(string.ascii_letters + string.digits) for n in range(4)]
+    )
     for i, chunk in enumerate(output_chunks):
-        t = (datetime.now() - datetime.now()).seconds
-        path = os.path.join(chunks_folder, f"{t}_{i}.wav")
+        path = os.path.join(chunks_folder, f"{title}_{i}.wav")
         chunk.export(path, format="wav")
         chunk_paths.append(path)
     return chunk_paths
 
 
 def transcribe_video(video_path):
-    transcription = ""
+    full_transcription = ""
     chunk_paths = create_audio_chunks(video_path)
 
-    client = OpenAI()()
+    client = OpenAI()
     for i, chunk_path in enumerate(chunk_paths):
-        with open(chunk_path, "rb") as f:
-            audio_bytes = f.read()
-
-        response = client.audio.transcriptions.create(
-            model="whisper-1", file=audio_bytes
+        audio_file = open(chunk_path, "rb")
+        transcription = client.audio.transcriptions.create(
+            model="whisper-1", file=audio_file, language="en"
         )
-        print(response["text"])
-        transcription += response["text"]
-        time.sleep(5 * i)
+        transcription += str(transcription.text)
 
-    print(transcription)
+    return full_transcription
