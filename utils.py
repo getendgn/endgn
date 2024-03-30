fbe5080836d7ba36c8a944d3a1dc1a11e7b1b0fa
nat <natnael08@protonmail.com>
2024-03-30 20:31:26 +0300

    update utils.py

--- utils.py
+++ utils.py
@@ -188,6 +188,6 @@ def upload_image(img_path):
     cloudinary.config(
         cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
         api_key=os.getenv("CLOUDINARY_API_KEY"),
-        api_secret=os.getenv("CLOUDINARY_API_KEY"),
+        api_secret=os.getenv("CLOUDINARY_API_SECRET"),
     )
     return cloudinary.uploader.upload(img_path)
