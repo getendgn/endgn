8f6de7624546fef0722c196e73d278a131cf6287
nat <natnael08@protonmail.com>
2024-04-07 15:48:08 +0300

    fix transcription

--- transcription.py
+++ transcription.py
@@ -1,41 +1,44 @@
 from pydub import AudioSegment
 from openai import OpenAI
 from logger import logger
-import os, random, string
+import os
 
 
-def create_audio_chunks(video_path):
+def create_audio_chunks(video_path, max_size=24):
     logger.info("Starting audio chunking")
+    audio_path = f"{video_path}.wav"
+    filename = os.path.basename(video_path).split(".")[0]
+    AudioSegment.from_file(video_path).export(audio_path, format="wav")
+    audio_size = os.path.getsize(audio_path) / (1024 * 1024)
+    num_chunks = int(audio_size / max_size) + 1
+
     chunks_folder = os.path.join(os.getcwd(), "tmp", "chunks")
     os.makedirs(chunks_folder, exist_ok=True)
 
-    audio = AudioSegment.from_file(video_path)
+    audio = AudioSegment.from_file(audio_path)
 
-    duration = 60000
-    chunk_paths = []
-    title = "".join(
-        [random.choice(string.ascii_letters + string.digits) for n in range(4)]
-    )
-    for i in range(len(audio) // duration):
-        start_time = i * duration
-        end_time = (i + 1) * duration
+    audio_chunks_path = []
+    for i in range(num_chunks):
+        start_time = i * len(audio) // num_chunks
+        end_time = (i + 1) * len(audio) // num_chunks
 
-        path = os.path.join(chunks_folder, f"{title}_{i}.wav")
-        segment = audio[start_time:end_time]
-        segment.export(path, format="wav")
-        chunk_paths.append(path)
+        audio_chunk = audio[start_time:end_time]
+        path = os.path.join(chunks_folder, f"{filename}_{i}.wav")
+        audio_chunks_path.append(path)
+        audio_chunk.export(path, format="wav")
 
+    os.unlink(audio_path)
     logger.info("Audio chunks created")
-    return chunk_paths
+    return audio_chunks_path
 
 
 def transcribe_video(video_path):
     full_transcription = ""
-    chunk_paths = create_audio_chunks(video_path)
+    chunk_path = create_audio_chunks(video_path)
 
     client = OpenAI()
-    for i, chunk_path in enumerate(chunk_paths):
-        audio_file = open(chunk_path, "rb")
+    for i, path in enumerate(chunk_path):
+        audio_file = open(path, "rb")
         transcription = client.audio.transcriptions.create(
             model="whisper-1",
             file=audio_file,
@@ -45,7 +48,7 @@ def transcribe_video(video_path):
         if transcription:
             full_transcription += transcription
 
-    for i in chunk_paths:
+    for i in chunk_path:
         os.unlink(i)
 
     logger.info("Transcription completed")
